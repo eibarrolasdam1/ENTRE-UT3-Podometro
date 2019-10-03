f59845bbfb8893f723a2ace5fda680f54d4e2c81
Eunate Ibarrola Santesteban (DAM 1) <eibarrolasdam1@gmail.com>
2019-10-03 16:56:24 +0200

    Hecho el constructor (los atributos y las constantes declaradas)

--- Podometro.java
+++ Podometro.java
@@ -3,31 +3,49 @@
  * acerca de los pasos, distancia, ..... que una persona (hombre o mujer)
  * ha dado en una semana. 
  * 
- * @author    - pon aquí tu nombre - 
+ * @author    - Eunate Ibarrola Santesteban - 
  * 
  */
 public class Podometro {
-   
+    private final char HOMBRE = 'H';
+    private final char MUJER = 'M';
+    private final double ZANCADA_HOMBRE = 0.45;
+    private final double ZANCADA_MUJER = 0.41;
+    private final int SABADO = 6;
+    private final int DOMINGO = 7;
     
-    
+    private String marca;
+    private double altura;
+    private char sexo;
+    private double longitudZancada;
+    private int totalPasosLaborables;
+    private int totalPasosSabado;
+    private int totalPasosDomingo;
+    private int tiempo;
+    private int caminatasNoche;
 
     /**
      * Inicializa el podómetro con la marca indicada por el parámetro.
      * El resto de atributos se ponen a 0 y el sexo, por defecto, es mujer
      */
-    public Podometro() {
-
-         
+    public Podometro(String queMarca) {
+        marca = queMarca;
+        altura = 0;
+        sexo = MUJER;
+        longitudZancada = 0;
+        totalPasosLaborables = 0;
+        totalPasosSabado = 0;
+        totalPasosDomingo = 0;
+        tiempo = 0;
+        caminatasNoche = 0;
     }
-
+    
     /**
      * accesor para la marca
      *  
      */
-    public    getMarca() {
-
-        
-
+    public String getMarca() {
+        return marca;
     }
 
     /**
@@ -41,10 +59,9 @@ public class Podometro {
      */
     public void configurar(double queAltura, char queSexo) {
 
-        
     }
-
-     /**
+    
+    /**
      *  Recibe cuatro parámetros que supondremos correctos:
      *    pasos - el nº de pasos caminados
      *    dia - nº de día de la semana en que se ha hecho la caminata 
@@ -58,13 +75,12 @@ public class Podometro {
      *   (leer enunciado del ejercicio)
      */
     public void registrarCaminata(int pasos, int dia, int horaInicio,
-                            int horaFin) {
+    int horaFin) {
 
-        
 
     }
-    
-     /**
+
+    /**
      * Muestra en pantalla la configuración del podómetro
      * (altura, sexo y longitud de la zancada)
      * 
@@ -73,7 +89,6 @@ public class Podometro {
      */
     public void printConfiguracion() {
 
-        
 
     }
 
@@ -86,23 +101,19 @@ public class Podometro {
      */
     public void printEstadísticas() {
 
-        
 
     }
 
-   
 
     /**
      *  Calcula y devuelve un String que representa el nombre del día
      *  en el que se ha caminado más pasos - "SÁBADO"   "DOMINGO" o  "LABORABLES"
      */
     public String diaMayorNumeroPasos() {
-
+        return marca;
         
-        
-
     }
-
+    
     /**
      * Restablecer los valores iniciales del podómetro
      * Todos los atributos se ponen a cero salvo el sexo
@@ -111,7 +122,6 @@ public class Podometro {
      */    
     public void reset() {
 
-        
 
     }
 
